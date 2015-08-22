09082a66cfdbea5d337b341e3df8eebdae341a0b
Lauren Beatty <lauren.m.beatty@gmail.com>
2015-08-22 17:13:53 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var avengersMovie = {
+    name : "Avengers",
+    runtime : "143 mins", //I assumed runtime was one word, instead of runTime
+    releaseYear: 2012,
+    director: "Joss Whedon"
+};
+console.log(avengersMovie);
